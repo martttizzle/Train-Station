a9856807aba42fe0701af7ee0f902b4a831d7aec
Vinod <ramdinv@gmail.com>
2018-04-27 19:34:48 -0500

    added the table and firebase is taking inputs

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
+
+    //Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBuS5uaU-hwIEWm71_VZgtZe_jt4jrke9E",
+        authDomain: "inclass-916ce.firebaseapp.com",
+        databaseURL: "https://inclass-916ce.firebaseio.com",
+        projectId: "inclass-916ce",
+        storageBucket: "inclass-916ce.appspot.com",
+        messagingSenderId: "174048502518"
+    };
+    firebase.initializeApp(config);
 
 
-        // Initialize Firebase
-        
-       
-          // Initialize Firebase
-          var config = {
-            apiKey: "AIzaSyBuS5uaU-hwIEWm71_VZgtZe_jt4jrke9E",
-            authDomain: "inclass-916ce.firebaseapp.com",
-            databaseURL: "https://inclass-916ce.firebaseio.com",
-            projectId: "inclass-916ce",
-            storageBucket: "inclass-916ce.appspot.com",
-            messagingSenderId: "174048502518"
-          };
-          firebase.initializeApp(config);
-  
-        firebase.initializeApp(config);
-        var database = firebase.database();
-        var Tname;
-        var mTime;
-        var startDate;
-        var monthlyR;
-    
-        $("#button").on("click", function () {
-            Tname = $("#Tname").val().trim();
-            mTime = $("#mTime").val().trim();
-            startDate = $("#startDate").val().trim();
-            monthlyR = $("#rate").val().trim();
-    
-            database.ref().push({
-    
-                Tname: Tname,
-                mTime: mTime,
-                startDate: startDate,
-                monthlyR: monthlyR
-    
-            });
+    var database = firebase.database();
+    var tName;
+    var dest;
+    var mTime;
+    var hTime;
+    var freq;
+
+
+
+    $("#btn").on("click", function (event) {
+        event.preventDefault();
+
+        tName = $("#tName").val().trim();
+        dest = $("#dest").val().trim();
+        hTime = $("#hTime").val().trim();
+        mTime = $("#mTime").val().trim();
+        freq = $("#freq").val().trim();
+
+        database.ref().push({
+            Name: tName,
+            Destination: dest,
+            Hours: hTime,
+            Minutes: mTime,
+            Frequency: freq
         });
-        database.ref().on("child_added", function (snapshot) {
-            Tname = snapshot.val().Tname;
-            mTime = snapshot.val().mTime;
-            startDate = snapshot.val().startDate;
-            monthlyR = snapshot.val().monthlyR;
-    
-            var tr = $("<tr>");
-            tr.append("<td>" + Tname);
-            tr.append("<td>" + mTime);
-            tr.append("<td>" + startDate);
-            tr.append("<td>"+ monthlyR);
-            $("#tb").append(tr);
-            console.log(tr);
-    
-        },
-        function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-        });
-    
-        
     });
+    
+    database.ref().on("child_added", function (snapshot) {
+        tName = snapshot.val().tName;
+        dest = snapshot.val().dest;
+        hTime = snapshot.val().hTime;
+        mTime = snapshot.val().mTime;
+        freq = snapshot.val().freq;
+
+        // var tr = $("<tr>");
+        // tr.append("<td>" + tName);
+        // tr.append("<td>" + mTime);
+        // tr.append("<td>" + hTime);
+        // tr.append("<td>"+ freq);
+        // $("#tb").append(tr);
+        // console.log(tr);
+
+    })
+});
 
 
 
